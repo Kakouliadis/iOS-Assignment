18af19ba0f896821047d506b397816bce8e3f08a
George Kakouliadis <gkakouli@netcompany-intrasoft.com>
2022-07-13 11:07:02 +0300

    Initial Commit

---
+++ iOS Assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Assignment
+//
+//  Created by Georgios Kakouliadis on 13/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Assignment/iOS_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_AssignmentApp.swift
+//  iOS Assignment
+//
+//  Created by Georgios Kakouliadis on 13/7/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
